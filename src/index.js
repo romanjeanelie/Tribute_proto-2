60f460ec4686afb46ee45a954410a2facb54378a
Roman <romanjeanelie@yahoo.fr>
2021-03-09 11:25:59 +0100

    try

--- src/index.js
+++ src/index.js
@@ -18,6 +18,26 @@ export default class Lines {
   anim() {
     console.log(document.getElementById("line-1"));
     this.tl1.from("#line-1", { drawSVG: 0, duration: 5 });
+
+    gsap.set(".line", { attr: { stroke: "hsl(1,100%, 50%)", "stroke-width": 4, "stroke-linecap": "round" } });
+    gsap.set(".line3", { attr: { "stroke-width": 9 }, opacity: 0.15 });
+
+    var shapes = "rect, circle, ellipse, polyline",
+      tl = gsap.timeline({ repeat: 1, yoyo: true });
+
+    tl.fromTo(shapes, { drawSVG: "100%" }, { duration: 1, drawSVG: "50% 50%", stagger: 0.1 })
+      .fromTo(shapes, { drawSVG: "0%" }, { duration: 0.1, drawSVG: "10%", immediateRender: false }, "+=0.1")
+      .to(shapes, { duration: 1, drawSVG: "90% 100%", stagger: 0.5 })
+      .to(shapes, {
+        duration: 1,
+        rotation: 360,
+        scale: 0.5,
+        drawSVG: "100%",
+        stroke: "white",
+        strokeWidth: 6,
+        transformOrigin: "50% 50%",
+      })
+      .to(shapes, { duration: 0.5, stroke: "red", scale: 1.5, opacity: 0, stagger: 0.2 });
   }
 }
 
