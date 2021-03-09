21a2b80f9873509ccc1322798cee146333ac01cd
Roman <romanjeanelie@yahoo.fr>
2021-03-09 11:40:17 +0100

    try

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ export default class Lines {
 
     this.tl1 = gsap.timeline({});
 
-    (this.shapes = "rect, circle, ellipse, polyline"), (this.tl = gsap.timeline({ repeat: 1, yoyo: true }));
+    this.shapes = "rect, circle, ellipse, polyline, line";
+    this.tl = gsap.timeline({ repeat: 1, yoyo: true });
 
     this.init();
   }
@@ -21,7 +22,7 @@ export default class Lines {
     console.log(document.getElementById("line-1"));
     this.tl1.from("#line-1", { drawSVG: 0, duration: 5 });
 
-    gsap.set(".line", { attr: { stroke: "hsl(1,100%, 50%)", "stroke-width": 4, "stroke-linecap": "round" } });
+    // gsap.set(".line", { attr: { stroke: "hsl(1,100%, 50%)", "stroke-width": 4, "stroke-linecap": "round" } });
     // gsap.set(".line-2", { attr: { "stroke-width": 9 }, opacity: 0.15 });
 
     this.tl.from(this.shapes, { duration: 1, drawSVG: 0, stagger: 0.1 });
